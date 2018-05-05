a847f2ba018ac44d2b418b880b58de61e45d6bff
Still Hsu <341464@gmail.com>
2018-05-05 18:19:23 +0800

    Change styling & fix incorrect date time

--- DocFx.Plugin.LastModified/LastModifiedPostProcessor.cs
+++ DocFx.Plugin.LastModified/LastModifiedPostProcessor.cs
@@ -12,31 +12,30 @@ namespace DocFx.Plugin.LastModified
     [Export(nameof(LastModifiedPostProcessor), typeof(IPostProcessor))]
     public class LastModifiedPostProcessor : IPostProcessor
     {
-        public ImmutableDictionary<string, object> PrepareMetadata(ImmutableDictionary<string, object> metadata)
-        {
-            return metadata;
-        }
+        public ImmutableDictionary<string, object> PrepareMetadata(ImmutableDictionary<string, object> metadata) =>
+            metadata;
 
         public Manifest Process(Manifest manifest, string outputFolder)
         {
             Logger.LogInfo("Begin adding last modified date to items...");
-            foreach (var manifestItem in manifest.Files.Where(x=>x.DocumentType == "Conceptual"))
+            foreach (var manifestItem in manifest.Files.Where(x => x.DocumentType == "Conceptual"))
             {
                 foreach (var manifestItemOutputFile in manifestItem.OutputFiles)
-                {
-                    AddModifiedDate(Path.Combine(outputFolder, manifestItemOutputFile.Value.RelativePath));
-                }
+                    AddModifiedDate(Path.Combine(manifest.SourceBasePath, manifestItem.SourceRelativePath),
+                        Path.Combine(outputFolder, manifestItemOutputFile.Value.RelativePath));
             }
+
             return manifest;
         }
 
-        private void AddModifiedDate(string path)
+        private void AddModifiedDate(string sourcePath, string outputPath)
         {
-            var lastWrittenDate = File.GetLastWriteTimeUtc(path);
+            var lastWrittenDate = File.GetLastWriteTimeUtc(sourcePath);
             if (lastWrittenDate == default(DateTimeOffset)) return;
+            Logger.LogInfo($"Writing {lastWrittenDate} from {sourcePath} to {outputPath}");
 
             var htmlDoc = new HtmlDocument();
-            htmlDoc.Load(path);
+            htmlDoc.Load(outputPath);
             var articleNode = htmlDoc.DocumentNode.SelectSingleNode("//article[contains(@class, 'content wrap')]");
             if (articleNode == null)
             {
@@ -44,23 +43,15 @@ namespace DocFx.Plugin.LastModified
                 return;
             }
 
-            var separator = htmlDoc.CreateElement("hr");
-
-            var noteNode = htmlDoc.CreateElement("div");
-            noteNode.SetAttributeValue("class", "NOTE");
-
-            var titleNode = htmlDoc.CreateElement("h5");
-            titleNode.InnerHtml = "Last Updated";
-            noteNode.AppendChild(titleNode);
-
             var paragraphNode = htmlDoc.CreateElement("p");
             paragraphNode.InnerHtml = $"This page was last modified at {lastWrittenDate} (UTC).";
-            noteNode.AppendChild(paragraphNode);
 
-            articleNode.AppendChild(separator);
-            articleNode.AppendChild(noteNode);
+            var separatorNode = htmlDoc.CreateElement("hr");
 
-            htmlDoc.Save(path);
+            articleNode.AppendChild(separatorNode);
+            articleNode.AppendChild(paragraphNode);
+
+            htmlDoc.Save(outputPath);
         }
     }
 }
