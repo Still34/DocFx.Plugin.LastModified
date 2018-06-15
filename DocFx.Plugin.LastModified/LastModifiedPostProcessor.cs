e363fd03a09fa090c486596a78e9261bf7b1ca1c
Still Hsu <341464@gmail.com>
2018-06-15 21:16:45 +0800

    Utilize git for last written date

--- DocFx.Plugin.LastModified/LastModifiedPostProcessor.cs
+++ DocFx.Plugin.LastModified/LastModifiedPostProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Immutable;
 using System.Composition;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -27,18 +28,45 @@ namespace DocFx.Plugin.LastModified
             foreach (var manifestItem in manifest.Files.Where(x => x.DocumentType == "Conceptual"))
             {
                 foreach (var manifestItemOutputFile in manifestItem.OutputFiles)
-                    AddModifiedDate(Path.Combine(manifest.SourceBasePath, manifestItem.SourceRelativePath),
-                        Path.Combine(outputFolder, manifestItemOutputFile.Value.RelativePath));
+                {
+                    string sourcePath = Path.Combine(manifest.SourceBasePath, manifestItem.SourceRelativePath);
+                    string outputPath = Path.Combine(outputFolder, manifestItemOutputFile.Value.RelativePath);
+                    DateTimeOffset lastModified;
+                    try
+                    {
+                        lastModified = GetCommitDate(sourcePath);
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.LogVerbose(e.ToString());
+                        Logger.LogVerbose("Failed to fetch commit date, falling back to system write time.");
+                        lastModified = GetWriteTimeFromFile(sourcePath);
+                    }
+                    WriteModifiedDate(lastModified, sourcePath, outputPath);
+                }
             }
-
             return manifest;
         }
 
-        private void AddModifiedDate(string sourcePath, string outputPath)
+        private DateTimeOffset GetCommitDate(string sourcePath)
         {
-            var lastWrittenDate = File.GetLastWriteTimeUtc(sourcePath);
-            if (lastWrittenDate == default(DateTimeOffset)) return;
-            Logger.LogInfo($"Writing {lastWrittenDate} from {sourcePath} to {outputPath}");
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = $"log -1 --format=%cd --date=iso8601 {sourcePath}",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            var process = System.Diagnostics.Process.Start(processStartInfo);
+            string output = process?.StandardOutput.ReadToEnd();
+            return DateTimeOffset.Parse(output);
+        }
+
+        private DateTimeOffset GetWriteTimeFromFile(string sourcePath) => File.GetLastWriteTimeUtc(sourcePath);
+
+        private void WriteModifiedDate(DateTimeOffset modifiedDate, string sourcePath, string outputPath)
+        {
+            Logger.LogInfo($"Writing {modifiedDate} from {sourcePath} to {outputPath}");
 
             var htmlDoc = new HtmlDocument();
             htmlDoc.Load(outputPath);
@@ -50,7 +78,7 @@ namespace DocFx.Plugin.LastModified
             }
 
             var paragraphNode = htmlDoc.CreateElement("p");
-            paragraphNode.InnerHtml = $"This page was last modified at {lastWrittenDate} (UTC).";
+            paragraphNode.InnerHtml = $"This page was last modified at {modifiedDate} (UTC).";
 
             var separatorNode = htmlDoc.CreateElement("hr");
 
